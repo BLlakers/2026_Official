274d63b767bd2fbe51a88c59f7d973aa532a2642
Sean Monaghan <smonaghan@pm.me>
2026-02-24 18:42:08 -0500

    remove legacy TeamSparkMax interface

--- src/main/java/frc/robot/support/sparkmax/TeamSparkMax.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.support.sparkmax;
-
-/**
- * @deprecated Use {@link TeamSpark} instead. This interface is kept for backward compatibility.
- */
-@Deprecated
-public interface TeamSparkMax extends TeamSpark {}
