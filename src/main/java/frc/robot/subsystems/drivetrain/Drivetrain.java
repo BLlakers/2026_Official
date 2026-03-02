c7a8e9bd9dbb76c443fbcda66e86091e85296324
Sean Monaghan <smonaghan@pm.me>
2026-03-02 18:03:43 -0500

    Fixed formatting

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -424,7 +424,8 @@ public class Drivetrain extends SubsystemBase {
             this.swerveDrivePoseEstimator.resetPosition(this.simYaw, simPositions, pose2d);
         } else {
             this.swerveDriveOdometry.resetPosition(this.getGyroRotation(), this.getSwerveModulePositions(), pose2d);
-            this.swerveDrivePoseEstimator.resetPosition(this.getGyroRotation(), this.getSwerveModulePositions(), pose2d);
+            this.swerveDrivePoseEstimator.resetPosition(
+                    this.getGyroRotation(), this.getSwerveModulePositions(), pose2d);
         }
     }
 
